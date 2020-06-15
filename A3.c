e645cc2eed71f55c082397db61ab00f954f09ffa
Anran Zhang <zhanganran2011@@live.com>
2020-06-15 17:23:15 -0300

    add debug and fcfs implementation

--- A3.c
+++ A3.c
@@ -13,12 +13,13 @@
 #include    <stdbool.h>
 #include    <string.h>
 #include    <math.h>
-#ifdef DEBUG
+//#ifdef DEBUG
 #define D_PRNT(...) fprintf(stderr, __VA_ARGS__)
+/*
 #else
 #define D_PRNT(...)
 #endif
-
+*/
 //define default values
 #define     DEFAULT_INIT_JOBS        5
 #define     DEFAULT_TOTAL_JOBS        100
@@ -168,6 +169,33 @@ double rand_exp(double lambda)
     raw_value = log(1 - u_0_to_almost_1) / -lambda;
     return round(raw_value * 1000000.) / 1000000.;
 }
+//define struct job
+struct Job {
+    //usec
+    int64_t start;
+    int64_t generated; // time when the job entered
+    int64_t compute_time;
+    int64_t passed_time;
+    int state; //running = 0,waiting = 1,finished = 2
+    int64_t wait_time;
+    int64_t response_time;
+    int64_t turnaround_time;
+};
+//function that generates a job and initialize it
+struct Job getJob(double lambda, int64_t time)
+{
+    struct Job j = {
+            .start = 0,
+            .generated = time,
+            .compute_time = rand_exp(lambda)*1000000,
+            .passed_time = 0,
+            .state = 1,
+            .wait_time = 0,
+            .response_time = 0,
+            .turnaround_time = 0
+    };
+    return j;
+}
 int main(int argc, char *argv[])
 {
     progname = argv[0];
@@ -185,29 +213,159 @@ int main(int argc, char *argv[])
 
     if (process_args(argc, argv, &sim_params) != 0)
         return EXIT_FAILURE;
-    //quit if no scheduling algorithm is specified
-    if (sim_params.sched_alg == UNDEFINED)
-    {
-        usage("No schedule algorithm is specified\n");
-        return EXIT_FAILURE;
-    }
+
     //set random flags
     if (sim_params.randomize == true)
         srandom(NULL);
-    //TODO: RUN THE SIMULATION HERE
-    struct Job {
-        int64_t start;
-        int64_t compute_time;
-        int64_t passed_time;
-        int state; //running = 0,waiting = 1
-    };
-    int64_t usec = 0;
-    struct Job jobs [sim_params.total_jobs];
-
+    if (sim_params.sched_alg == UNDEFINED)
+    {
+        //quit if no scheduling algorithm is specified
+        usage("No schedule algorithm is specified\n");
+        return EXIT_FAILURE;
+    }
     //TODO: Calculate statistics
-    double average_response_time = 0.42;
-    double average_waiting_time = 0.53;
-    double average_turnaround_time = 98.1;
+    double average_response_time = 0;
+    double average_waiting_time = 0;
+    double average_turnaround_time = 0;
+
+    //TODO: RUN THE SIMULATION HERE
+
+    int64_t clock_usec = -1;
+    struct Job jobs [sim_params.total_jobs];
+    int job_count = 0;
+    int finished_jobs = 0;
+    //initialize the jobs
+    for (int i = 0; i < sim_params.init_jobs; i++)
+    {
+        jobs[i] = getJob(sim_params.lambda, 0);
+        D_PRNT("t=%d,job %d is added, needing %ld usec\n",0,i,jobs[i]
+        .compute_time);
+
+    }
+    job_count += sim_params.init_jobs;
+    int64_t scheduler_start_time = 0; //the time a scheduler starts
+    int64_t cs_start_time = 0; //the time a context switch starts
+    bool scheduler_running = false;
+    bool context_switch_running = false;
+    //to keep track of the jobs
+    int previous_job_index = 0;
+    int current_job_index = 0;
+    while (finished_jobs<sim_params.total_jobs)
+    {
+        clock_usec++;//increments time in usec
+        if (clock_usec%(sim_params.tick_time*1000)==0)
+        {
+            //clock tick and runs the scheduler
+            //if the current job is running, it is stopped
+            //D_PRNT("t=%ld clock ticks, scheduler running\n",clock_usec);
+            if (jobs[current_job_index].state==0)
+            {
+                D_PRNT("t=%ld,clock ticks,current running process %d stops\n",
+                        clock_usec,
+                 current_job_index);
+                jobs[current_job_index].state = 1;
+                //jobs[current_job_index].wait_time++;
+            }
+            scheduler_running = true;
+            scheduler_start_time = clock_usec;
+        }
+        //scheduler is running
+        if (scheduler_running)
+        {
+            if (clock_usec != scheduler_start_time + sim_params.sched_time)
+                continue;
+            else
+            {
+                scheduler_running = false;//scheduler finish
+                D_PRNT("t=%ld,scheduler done\n",clock_usec);
+            }
+        }
+        //context switch is running
+        if (context_switch_running &&clock_usec != cs_start_time + sim_params
+        .cont_swtch_time)
+        {
+            continue;
+        }
+        if (clock_usec == cs_start_time + sim_params.cont_swtch_time) {
+            context_switch_running = false;//cs finish
+            D_PRNT("t=%ld,context switch done\n",clock_usec);
+        }
+        //if the current job is running
+        if (jobs[current_job_index].state==0)
+        {
+            //increment time count
+            jobs[current_job_index].passed_time++;
+            jobs[current_job_index].turnaround_time++;
+            //if at current time the job finishes
+            if (jobs[current_job_index].passed_time ==
+            jobs[current_job_index].compute_time)
+            {
+                //current job finishes
+                jobs[current_job_index].state=2;
+                finished_jobs++;
+                //adds to statistics in seconds
+                average_response_time += (double)
+                        jobs[current_job_index].response_time/sim_params
+                        .total_jobs/1000000;
+                average_turnaround_time += (double)
+                        jobs[current_job_index].turnaround_time/sim_params
+                        .total_jobs/1000000;
+                average_waiting_time += (double)
+                        jobs[current_job_index].wait_time/sim_params
+                        .total_jobs/1000000;
+                previous_job_index = current_job_index;
+                D_PRNT("process %d finished at %ld usec\n",
+                       current_job_index,clock_usec);
+            }
+        }
+        //Todo: generate new job
+
+        if (sim_params.sched_alg == FCFS)
+        {
+            //FCFS
+            if (jobs[current_job_index].state == 1)
+            {
+                jobs[current_job_index].state = 0;
+                D_PRNT("t=%ld,dispatching process %d,needing %ld usec\n",
+                        clock_usec, current_job_index, jobs[current_job_index]
+                               .compute_time);
+            }
+            if (jobs[current_job_index].state == 2 && job_count >
+                                                      current_job_index + 1)
+            {
+                //previous job finished and there are jobs left
+                current_job_index++;
+                jobs[current_job_index]
+                        .response_time = clock_usec - jobs[current_job_index]
+                        .generated;
+
+                //runs scheduler at next usec
+                scheduler_running = true;
+                scheduler_start_time = clock_usec;
+                //runs context switch after the scheduler finish
+                context_switch_running = true;
+                cs_start_time = scheduler_start_time + sim_params.sched_time;
+                continue;
+            }
+        }
+        if (sim_params.sched_alg == SJF)
+        {
+            D_PRNT("Running sjf\n");
+        }
+        if (sim_params.sched_alg == RR) {
+            D_PRNT("Running rr\n");
+        }
+        //count the wait and turnaround time for the process in the queue
+        //note if the scheduler or context switch is going on, it won't get here
+        for (int i = 0; i < job_count; ++i) {
+            if (jobs[i].state==1)
+            {
+                jobs[i].wait_time++;
+                jobs[i].turnaround_time++;
+            }
+        }
+    }
+
 
     //Print info using provided code
     printf("For a simulation using the %s scheduling algorithm\n",
